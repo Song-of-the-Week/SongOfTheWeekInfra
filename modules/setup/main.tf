df4b0ef5194ba5279169169775154d6c616d2bed
Reddig <crreddig@gmail.com>
2024-02-13 19:41:55 -0500

    Basic boilerplate setup

---
+++ modules/setup/main.tf
@@ -0,0 +1,4 @@
+# stage/frontend-app/main.tf
+# variable "assume_role_arn" {
+#   description = "Role to assume for AWS API calls"
+# }
---
+++ modules/setup/variables.tf
@@ -0,0 +1,4 @@
+variable "assume_role_arn" {
+  description = "Role to assume for AWS API calls"
+  default = "arn:aws:iam::471112828417:role/terragrunt"
+}
