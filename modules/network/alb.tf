b42d37c54ede318a4764ae59f01f4e5e098c7dbd
Reddig <crreddig@gmail.com>
2024-12-31 09:57:08 -0500

    Improve EC2 and ECS security (#16)

--- modules/network/alb.tf
+++ modules/network/alb.tf
@@ -3,7 +3,7 @@ resource "aws_lb" "ecs_alb" {
   name               = "ecs-alb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.ecs.id]
+  security_groups    = [aws_security_group.alb.id]
   subnets            = [aws_subnet.ecs_1a.id, aws_subnet.ecs_1b.id]
 
   tags = {
