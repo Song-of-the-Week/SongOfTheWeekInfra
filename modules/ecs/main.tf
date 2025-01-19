75cdaa31ddeb599e8fbaa70a0cbf4324a0397073
Reddig <crreddig@gmail.com>
2025-01-19 01:15:40 -0500

    change frontend health check

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -171,7 +171,7 @@ resource "aws_ecs_task_definition" "this" {
         { name = "VUE_APP_SPOTIFY_CALLBACK_URI", value = "https://${local.domain_name}/" },
       ]
       healthCheck = {
-        command     = ["CMD-SHELL", "curl -f http://localhost:8080/health || exit 1"]
+        command     = ["CMD-SHELL", "curl -f http://localhost:8080/ || exit 1"]
         interval    = 30
         timeout     = 5
         retries     = 3
