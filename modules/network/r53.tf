39407c1f5f22e77b29f82e48f8dfcc5a18d139fd
Reddig <crreddig@gmail.com>
2025-01-19 13:34:33 -0500

    www

--- modules/network/r53.tf
+++ modules/network/r53.tf
@@ -18,6 +18,21 @@ resource "aws_route53_record" "eip_records" {
   health_check_id = aws_route53_health_check.this.id # Attach the health check to the DNS record
 }
 
+
+resource "aws_route53_record" "eip_records_www" {
+  zone_id        = data.aws_route53_zone.this.zone_id # Replace with your Route 53 hosted zone ID
+  name           = "www.${var.domain_name}"           # Replace with your domain or subdomain
+  type           = "A"                                # For IPv4 address
+  ttl            = 60
+  set_identifier = "Primary"
+  failover_routing_policy {
+    type = "PRIMARY"
+  }
+  # Combine all Elastic IPs into a single list of records
+  records         = [for eip in aws_eip.this : eip.public_ip]
+  health_check_id = aws_route53_health_check.this.id # Attach the health check to the DNS record
+}
+
 resource "aws_route53_health_check" "this" {
   fqdn              = var.domain_name # The domain name to check
   type              = "HTTPS"         # You can use HTTP, HTTPS, TCP, etc.
